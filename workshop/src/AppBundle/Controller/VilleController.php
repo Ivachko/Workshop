d6d70f7e9061ad0a3d826e7a6cf7a0c9aae9c83c
Pierre <pierre.strobel@epsi.fr>
2017-03-14 12:13:25 +0100

    Ajout 2

--- workshop/src/AppBundle/Controller/VilleController.php
+++ workshop/src/AppBundle/Controller/VilleController.php
@@ -2,12 +2,40 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Ville;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class VilleController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * @Route("/Ville",name="indexVille")
+     * @param $name
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
     {
-        return $this->render('', array('name' => $name));
+        $em = $this->getDoctrine()->getEntityManager()->getRepository("AppBundle:Ville");
+        $s= $em->findAll();
+
+        return $this->render('Ville/index.html.twig',['listVille' => $s]);
+    }
+
+    /**
+     * @Route("/Ville/new",name="ajouterVille")
+     */
+    public function add(Request $request){
+        $ville= new Ville();
+        $form= $this->createForm(\AppBundle\Form\VilleType::class,$ville);
+        if($form->handleRequest($request)->isValid()){
+            $em= $this->getDoctrine()->getManager();
+            $em->persist($ville);
+            $em->flush();
+            $this->addFlash('Success','Ville '.$ville->getNom().' add');
+            return $this->redirectToRoute("indexVille");
+        }
+        return $this->render("Ville/new.html.twig",["form"=>$form->createView()]);
+
     }
 }
