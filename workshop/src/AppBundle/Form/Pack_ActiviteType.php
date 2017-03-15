2b8b51258c8b3671b623d37f05a3fd201b2dd764
Pierre <pierre.strobel@epsi.fr>
2017-03-15 10:05:50 +0100

    STR12MPC101214

--- workshop/src/AppBundle/Form/Pack_ActiviteType.php
+++ workshop/src/AppBundle/Form/Pack_ActiviteType.php
@@ -8,23 +8,18 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ActiviteType extends AbstractType
+class Pack_ActiviteType extends AbstractType
 {
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom')->add('adresse')->add('responsable')->add('telephone')->add('mail')->add('prixPersonne')->add('reduction')
-            ->add('Ville', EntityType::class, array(
-                'class'        => 'AppBundle\Entity\Ville',
-                'choice_label' => 'Nom',
-                'multiple'     => false))
-            ->add('Categories', EntityType::class, array(
-                'class'        => 'AppBundle\Entity\Categorie',
-                'choice_label' => 'Nom',
-                'multiple'     => true,
-                'expanded'     => true))
+        $builder->add('nom')
+            ->add('activites', EntityType::class, array(
+            'class'        => 'AppBundle\Entity\Activite',
+            'choice_label' => 'Nom',
+            'multiple'     => true))
             ->add("Enregister",SubmitType::class,["attr"=>["class"=>"btn btn-success"]]);
     }
     
@@ -34,7 +29,7 @@ class ActiviteType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Activite'
+            'data_class' => 'AppBundle\Entity\Pack_Activite'
         ));
     }
 
@@ -43,7 +38,7 @@ class ActiviteType extends AbstractType
      */
     public function getBlockPrefix()
     {
-        return 'appbundle_activite';
+        return 'appbundle_pack_activite';
     }
 
 
