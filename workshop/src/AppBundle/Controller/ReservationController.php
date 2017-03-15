7c225ef019fce3e40285f1f6ef375e3b0f2f13bf
Pierre <pierre.strobel@epsi.fr>
2017-03-15 11:54:05 +0100

    Ajout resvation resto (vue pas fonctionnelle mdr lol cesar jtm)

---
+++ workshop/src/AppBundle/Controller/ReservationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ReservationController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('', array('name' => $name));
+    }
+    /**
+     * @Route("/Reservation/restaurant-{nom}",name="reserverRestaurant")
+     */
+    public function  restaurantAction($nom,Request $request){
+        $em= $this->getDoctrine()->getEntityManager();
+
+        $restaurant=$em->getRepository("AppBundle:Restaurant")
+            ->findOneBy(['nom'=>$nom]);
+
+
+           return $this->render('@App/Reservation/restaurant.html.twig',['rest'=>$restaurant]);
+    }
+}
